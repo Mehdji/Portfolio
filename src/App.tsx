b02034549c3a242c2e03f3b8be7b6b99e1e0eb29
Mehdi <naouimehdi@hotmail.fr>
2025-05-06 20:11:18 +0200

    Refactor code structure for improved readability and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 //BTS SIO SLAM 2024
 import React from "react";
 import About from "./components/About";
+import Certificats from "./components/Certificats";
 import Contact from "./components/Contact";
 import Internship from "./components/Internship";
 import Navbar from "./components/Navbar";
@@ -11,7 +12,7 @@ import Skillsmatrix from "./components/Skillsmatrix";
 
 export default function App(): React.ReactNode {
   return (
-    <main className="text-gray-400 bg-blue-100 dark:bg-gray-900 scroll-smooth">
+    <main className="transition-colors duration-500  text-gray-400 bg-blue-100 dark:bg-gray-900 scroll-smooth">
       {/*<button
         className="ml-4 inline-flex font-title text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
         onClick={toggleDarkmode}
@@ -21,10 +22,11 @@ export default function App(): React.ReactNode {
       </button>*/}
       <Navbar />
       <About />
-      {/*<Certificats />*/}
+
       <Internship />
       <Skillsmatrix />
       <Skills />
+      <Certificats />
       <Project />
       <Contact />
       {/*<TechnologyMonitoring />*/}
